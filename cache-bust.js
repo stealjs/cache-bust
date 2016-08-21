29d9b8b817b754cd97bbdd121bdcbebce685ba71
Eben <me@ebenroux.co.za>
2016-08-21 18:10:29 +0200

    - removed cache-bust condition

--- cache-bust.js
+++ cache-bust.js
@@ -1,16 +1,14 @@
 var loader = require("@loader");
 var fetch = loader.fetch;
 var timestamp = new Date().getTime();
-var cacheVersion = isProduction() ? loader.cacheVersion || timestamp : timestamp;
-var cacheKey = loader.cacheKey || "version";
-var cacheKeyVersion = cacheKey + "=" + cacheVersion;
 
 loader.fetch = function(load) {
-	if(!load.metadata.plugin || load.metadata.cacheInitial) {
-		load.address = load.address + (load.address.indexOf('?') === -1 ? '?' : '&') + cacheKeyVersion;
-	} else if(load.metadata.plugin) {
-		load.metadata.cacheInitial = true;
-	}
+	var loader = this;
+	var cacheVersion = isProduction() ? loader.cacheVersion || timestamp : timestamp;
+	var cacheKey = loader.cacheKey || "version";
+	var cacheKeyVersion = cacheKey + "=" + cacheVersion;
+
+	load.address = load.address + (load.address.indexOf('?') === -1 ? '?' : '&') + cacheKeyVersion;
 
 	return fetch.call(this, load);
 };
