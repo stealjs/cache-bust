077c74262605998ddbbedb675046cfaeafc71f60
Eben <me@ebenroux.co.za>
2016-08-23 07:04:54 +0200

    - fixed build issue

--- cache-bust.js
+++ cache-bust.js
@@ -2,14 +2,24 @@ var loader = require("@loader");
 var fetch = loader.fetch;
 var timestamp = new Date().getTime();
 
+var isBuildEnvironment = loader.isPlatform ?
+	(loader.isPlatform("build") || loader.isEnv("build")) :
+	(typeof window === "undefined");
+
 loader.fetch = function(load) {
 	var loader = this;
-	var cacheVersion = isProduction() ? loader.cacheVersion || timestamp : timestamp;
-	var cacheKey = loader.cacheKey || "version";
-	var cacheKeyVersion = cacheKey + "=" + cacheVersion;
+	var cacheVersion;
+	var cacheKey;
+	var cacheKeyVersion;
+	
+	if (!isBuildEnvironment) {
+		cacheVersion = isProduction() ? loader.cacheVersion || timestamp : timestamp;
+		cacheKey = loader.cacheKey || "version";
+		cacheKeyVersion = cacheKey + "=" + cacheVersion;
 
-	load.address = load.address + (load.address.indexOf('?') === -1 ? '?' : '&') + cacheKeyVersion;
-
+		load.address = load.address + (load.address.indexOf('?') === -1 ? '?' : '&') + cacheKeyVersion;
+	}
+	
 	return fetch.call(this, load);
 };
 	
